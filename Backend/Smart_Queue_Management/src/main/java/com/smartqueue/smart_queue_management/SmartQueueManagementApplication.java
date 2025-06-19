6475aa665c889bfee54297e4034739f665e2394d
Dulan Fernando <dulanfernando0719@gmail.com>
2025-06-19 13:16:06 +0530

    Backend - 1

---
+++ Backend/Smart_Queue_Management/src/main/java/com/smartqueue/smart_queue_management/SmartQueueManagementApplication.java
@@ -0,0 +1,13 @@
+package com.smartqueue.smart_queue_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartQueueManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartQueueManagementApplication.class, args);
+    }
+
+}
